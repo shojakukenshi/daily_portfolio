83b1e96a59fd00d6a7b6c7ec1eb16bec646129b5
Kenshi Shojaku <shojaku@gmail.com>
2019-07-02 17:52:12 +0900

    fix post structure

--- app/controllers/login/posts_controller.rb
+++ app/controllers/login/posts_controller.rb
@@ -4,7 +4,7 @@ class Login::PostsController < Login::ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = current_user.posts.all
   end
 
   # GET /posts/1
@@ -14,7 +14,7 @@ class Login::PostsController < Login::ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   # GET /posts/1/edit
@@ -24,7 +24,7 @@ class Login::PostsController < Login::ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
@@ -64,7 +64,7 @@ class Login::PostsController < Login::ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = current_user.posts.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # root to: "home#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   namespace :login do
     resources :posts
   end
-  resources :posts
+  resources :posts, only: [:index, :show]
 end
